93a90a0045ac5e50595a40d0ba148062dbf5f03a
lefmarna <panume5ya@gmail.com>
2021-06-21 15:55:10 +0900

    ルーティングの設定

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PrototypeController@index');
 
 Auth::routes();
-
-// Route::get('/home', 'HomeController@index')->name('home');
-Route::get('prototypes', 'PrototypeController@index');
