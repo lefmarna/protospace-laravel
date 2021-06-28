7df069f3baf1d83cefc4d2cc592e29882ee955c3
lefmarna <panume5ya@gmail.com>
2021-06-28 17:36:08 +0900

    コメント用のモデルを追加

---
+++ database/migrations/2021_06_28_172939_create_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('text');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('prototype_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('prototype_id')->references('id')->on('prototypes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
