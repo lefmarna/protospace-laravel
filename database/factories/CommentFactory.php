f283656cdffc674c3427ce9f6b6c746c1ccc1e8d
lefmarna <panume5ya@gmail.com>
2021-06-30 21:29:30 +0900

    commentsのダミーデータを作成

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Comment;
+use Faker\Generator as Faker;
+
+$factory->define(Comment::class, function (Faker $faker) {
+    return [
+        'text' => $faker->realText(30),
+        'user_id' => $faker->numberBetween($min = 1, $max = 2),
+        'prototype_id' => $faker->numberBetween($min = 1, $max = 200),
+    ];
+});
