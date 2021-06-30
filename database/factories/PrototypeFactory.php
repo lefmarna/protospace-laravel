9ed88a1cd91115987d401d771a311921cd626fb5
lefmarna <panume5ya@gmail.com>
2021-06-30 21:55:29 +0900

    文字数を再調整

--- database/factories/PrototypeFactory.php
+++ database/factories/PrototypeFactory.php
@@ -9,7 +9,7 @@ $factory->define(Prototype::class, function (Faker $faker) {
     return [
         'title' => $faker->realText(10),
         'catch_copy' => $faker->realText(50),
-        'concept' => $faker->realText(190),
+        'concept' => $faker->realText(85),
         'user_id' => $faker->numberBetween($min = 1, $max = 2),
     ];
 });
