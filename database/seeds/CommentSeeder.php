5ed533b7ccdc852ced5a948fea4052a9a5171d8d
lefmarna <panume5ya@gmail.com>
2021-07-03 21:34:12 +0900

    seedデータの削減

--- database/seeds/CommentSeeder.php
+++ database/seeds/CommentSeeder.php
@@ -12,6 +12,6 @@ class CommentSeeder extends Seeder
      */
     public function run()
     {
-        factory(Comment::class, 3000)->create();
+        factory(Comment::class, 1000)->create();
     }
 }
